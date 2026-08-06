486a7c25dbdeead67da51c3cc9d63cc71ad9b7c7
Tanapat Chaithong <tanapatchaithong23@gmail.com>
2026-08-06 21:19:24 +0700

    feat(week-01): migrate developer environment lab

---
+++ labs/week-01/source/src/hello.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "Tanapat Chaithong",
+  studentId: "68543210064-0",
+  os: process.platform,
+  node: process.version,
+};
+
+function createGreeting({ name, studentId, os, node }) {
+  return `Hello ${name} (${studentId}) | OS: ${os} | Node: ${node}`;
+}
+
+console.log(createGreeting(student));
